2a687fb559cda721357d59d472744ba05a5605ef
Veysel Usta <veyselusta7@gmail.com>
2020-12-16 03:04:16 +0300

    Create Notification

---
+++ Notification.js
@@ -0,0 +1,12 @@
+class Notification {
+  constructor(){
+    this.userTags = []
+    this.mentions = []
+    this.retweetBy = []
+    this.likes = []
+    this.messages = []
+    this.follows = []
+  }
+}
+
+module.exports = Notification
