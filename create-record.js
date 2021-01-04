571f755ae8b316474f4cde2bea8447998b125517
Veysel Usta <veyselusta7@gmail.com>
2021-01-04 14:24:59 +0300

    Add create-record

---
+++ create-record.js
@@ -0,0 +1,40 @@
+const User = require('./models/User.js')
+const userDatabase = require('./database/user-database.js')
+
+
+const veysel = new User({
+  name : 'veysel',
+  username : 'veysel15',
+  mail : 'vyslusta1@gmail.com',
+  bio : 'hello twitter'
+})
+
+const kutlay = new User({
+  name : 'kutlay',
+  username : 'kutlay_12',
+})
+
+
+
+
+veysel.tweet('hello world')
+veysel.tweet('everything is gonna be alright')
+
+kutlay.tweet('hello @veysel')
+kutlay.tweet('i love javascript')
+
+veysel.follow(kutlay)
+kutlay.follow(veysel)
+
+veysel.reply(kutlay.tweets[1], 'You were supposed to write JavaScript respect please')
+
+veysel.retweet(kutlay.tweets[1])
+
+veysel.like(kutlay.tweets[0])
+
+veysel.pinTweet(veysel.tweets[1])
+
+veysel.sendDirectMessage('how are you today')
+kutlay.sendDirectMessage('i am good what about you')
+
+userDatabase.save([veysel,kutlay])
