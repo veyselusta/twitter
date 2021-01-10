cfa1bdb747a24d0f435e313bcd97902dbbc92abd
Veysel Usta <veyselusta7@gmail.com>
2021-01-10 03:11:54 +0300

    Make other BaseDatabase methods asynchronous

--- database/base-database.js
+++ database/base-database.js
@@ -24,27 +24,27 @@ class BaseDatabase {
     })
   }
   
-  insert(object) {
-    const objects = this.load()
-    this.save(objects.concat(object))
+  async insert(object) {
+    const objects = await this.load()
+    return save(objects.concat(object))
   }
 
-  find(id) {
-    return this.load().find(o => o.id == id)
+  async find(id) {
+    return await (this.load()).find(o => o.id == id)
   }
 
-  update(object) {
-    const objects = this.load()
+  async update(object) {
+    const objects = await this.load()
     const index = objects.findIndex(o => o.id == object.id)
     
     objects.splice(index, 1, object)
-    this.save(objects)
+    return this.save(objects)
   }
   
-  remove (index) {
-    const objects = this.load()
+  async remove (index) {
+    const objects = await this.load()
     objects.splice(index,1)
-    this.save(objects)
+    return this.save(objects)
   }
 
 }
