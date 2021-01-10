3ab4cf48217e48c72b1816eb052a8f8068a4d6f3
Veysel Usta <veyselusta7@gmail.com>
2021-01-10 03:51:28 +0300

    Make asynchronous findByName

--- database/user-database.js
+++ database/user-database.js
@@ -3,8 +3,8 @@ const User = require('../models/User')
 
 class UserDatabase extends BaseDatabase {
 
-  findByName(name) {
-    const data = this.load()
+  async findByName(name) {
+    const data = await this.load()
 
     return data.find(obj=> obj.name == name)
   }
