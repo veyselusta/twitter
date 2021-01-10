290d5d9e32a60e3837c0263a8ce9dad505a41968
Veysel Usta <veyselusta7@gmail.com>
2021-01-10 04:05:02 +0300

    Fix typo

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const User = require('./models/User.js')
 const userDatabase = require('./database/user-database.js')
 
 async function run(){
-  const veysel = await UserDatabase.findByName('veysel')
+  const veysel = await userDatabase.findByName('veysel')
   veysel.tweet('new tweet')
-  await UserDatabase.update(veysel)
+  await userDatabase.update(veysel)
+  console.log(veysel)
 }
 run()
 
