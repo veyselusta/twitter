1d7eab1bc35d6170b15674d4b5bcd4f75a748d75
Veysel Usta <veyselusta7@gmail.com>
2021-01-21 09:08:21 +0300

    Update Message and Notification

--- models/Message.js
+++ models/Message.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose')
 
 const MessageSchema = new mongoose.Schema({
   message : String,
-  author : Object,
+  author : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'User',
+    autopopulate : { maxDepth : 1 }
+  },
   createdAt : String
 })
+
+MessageSchema.plugin(require('mongoose-autopopulate'))
 
 module.exports = mongoose.model('Message', MessageSchema)
--- models/Notification.js
+++ models/Notification.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose')
 
 const NotificationSchmea = new mongoose.Schema({
-  author : Object,
+  author : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'User',
+    autopopulate : { maxDepth : 1 }
+  },
   item : Object,
   type : String
 })
+
+NotificationSchema.plugin(require('mongoose-autopopulate'))
 
 module.exports = mongoose.model('Notification', NotificationSchmea)
