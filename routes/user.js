1b960acfd0cc23e5c194fdec6a0dd5542d1026c3
Veysel Usta <veyselusta7@gmail.com>
2021-01-21 08:53:22 +0300

    Update routes

--- routes/user.js
+++ routes/user.js
@@ -1,66 +1,63 @@
-const userDatabase = require('../database')
+const {userService, tweetService} = require('../services')
 const User = require('../models/User')
 
 const router = require('express').Router()
 
-
 router.get('/:userId', async (req,res)=>{
-  const user = await userDatabase.find(req.params.userId)
+  const user = await userService.find(req.params.userId)
   if(!user) res.send('oww')
 
   res.render('user', { user })
 })
 
 router.get('/:userId/followers', async (req,res)=>{
-  const user = await userDatabase.find(req.params.userId)
+  const user = await userService.find(req.params.userId)
   const followers = user.followers
 
   res.render('followers', { followers })
 })
 
 router.get('/:userId/followings', async (req,res)=>{
-  const user = await userDatabase.find(req.params.userId)
+  const user = await userService.find(req.params.userId)
   const followings = user.following
 
   res.render('followings', { followings })
 })
 
 router.get('/:userId/likes', async (req,res)=>{
-  const user = await userDatabase.find(req.params.userId)
+  const user = await userService.find(req.params.userId)
   const likes = user.likedTweets
 
   res.render('likes', { likes })
 })
 
 router.get('/:userId/reply', async (req,res)=>{
-  const user = await userDatabase.find(req.params.userId)
+  const user = await userService.find(req.params.userId)
   const replies = user.replies
 
   res.render('replies', { replies })
+})
+
+router.post('/create', async(req,res)=>{
+  const user = await userService.insert(req.body)
+
+  res.send(user)
 })
 
 router.post('/:userId/reply', async(req,res)=>{
   const { userId } = req.params
   const { text, tweetId } = req.body
 
-  const user = await userDatabase.find(userId)
-  const tweet = await userDatabase.findTweetById(tweetId)
-
-  user.reply(tweet, text)
-  await userDatabase.update(user)
+  await user.reply(userId, tweetId)
 
   res.send('ok')
 })
 
-
 router.post('/:userId/follow', async (req,res)=>{
-  const user = await userDatabase.find(req.params.userId)
-  const other = await userDatabase.find(req.body.otherId)
+  const { userId } = req.params
+  const { otherId } = req.body
 
-  user.follow(other)
-  
-  await userDatabase.update(user)
-  await userDatabase.update(other)
+  await userService.follow(userId, otherId)
 
   res.send('ok')
 })
@@ -69,13 +66,7 @@ router.post('/:userId/like', async (req,res)=>{
   const { userId } = req.params
   const { tweetId } = req.body
 
-  const user = await userDatabase.find(userId)
-  const tweet = await userDatabase.findTweetById(tweetId)
-
-  if(!user || !tweet) throw new Error('hata')
-
-  user.like(tweet)
-  userDatabase.update(user)
+  await userService.like(userId, tweetId)
   
   res.send('ok')
 })
@@ -84,13 +75,23 @@ router.post('/:userId/tweet', async (req,res)=>{
   const { userId } = req.params
   const { text } = req.body
 
-  const user = await userDatabase.find(userId)
+  const tivit = await userService.tweet(text, userId)
 
-  user.tweet(text)
-  userDatabase.update(user)
+  res.send(tivit)
+})
+
+router.patch('/:userId', async (req,res)=>{
+  const { userId } = req.params
+  const { name } = req.body
   
+  await userService.update(userId, { name })
   res.send('ok')
 })
 
+router.delete('/:userId/delete', async (req,res)=>{
+  await userService.removeBy('_id', req.params.userId)
+
+  res.send('ok')
+})
 
 module.exports = router
